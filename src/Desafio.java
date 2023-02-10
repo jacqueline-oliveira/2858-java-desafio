6f21b16adfa4d43bd847ad435015a19d4315c26d
Jacqueline Oliveira <jacqueline.alterdata@gmail.com>
2023-02-10 12:33:38 -0300

    video 4.2 - resolução do desafio parte

---
+++ src/Desafio.java
@@ -0,0 +1,13 @@
+public class Desafio {
+    public static void main(String[] args) {
+        String nome = "Clark Kent";
+        String tipoConta = "Corrente";
+        double saldo = 1599.99;
+
+        System.out.println("***********************");
+        System.out.println("\nNome do cliente: " + nome);
+        System.out.println("Tipo conta: " + tipoConta);
+        System.out.println("Saldo atual: " + saldo);
+        System.out.println("\n***********************");
+    }
+}
