c3936346a9d2a80e545eb4ccaa0ed476a06c5c3c
Jacqueline Oliveira <jacqueline.alterdata@gmail.com>
2023-02-10 12:38:55 -0300

    video 4.3 - resolução parte 2

--- src/Desafio.java
+++ src/Desafio.java
@@ -1,13 +1,31 @@
+import java.util.Scanner;
+
 public class Desafio {
     public static void main(String[] args) {
         String nome = "Clark Kent";
         String tipoConta = "Corrente";
         double saldo = 1599.99;
+        int opcao = 0;
 
         System.out.println("***********************");
         System.out.println("\nNome do cliente: " + nome);
         System.out.println("Tipo conta: " + tipoConta);
         System.out.println("Saldo atual: " + saldo);
         System.out.println("\n***********************");
+
+        String menu = """
+                ** Digite sua opção **
+                1 - Consultar saldo
+                2 - Transferir valor
+                3 - Receber valor 
+                4 - Sair
+                
+                """;
+        Scanner leitura = new Scanner(System.in);
+
+        while (opcao != 4) {
+            System.out.println(menu);
+            opcao = leitura.nextInt();
+        }
     }
 }
