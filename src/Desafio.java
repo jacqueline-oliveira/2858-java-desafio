a7af002a4b3021080accf3f12350ca8c68ddb0aa
Jacqueline Oliveira <jacqueline.alterdata@gmail.com>
2023-02-10 14:39:21 -0300

    video 4.4 - resolução parte 3

--- src/Desafio.java
+++ src/Desafio.java
@@ -26,6 +26,26 @@ public class Desafio {
         while (opcao != 4) {
             System.out.println(menu);
             opcao = leitura.nextInt();
+
+            if (opcao == 1){
+                System.out.println("O saldo atualizado é " + saldo);
+            } else if (opcao == 2) {
+                System.out.println("Qual o valor que deseja transferir?");
+                double valor = leitura.nextDouble();
+                if (valor > saldo) {
+                    System.out.println("Não há saldo para realizar a transferência.");
+                } else {
+                    saldo -= valor;
+                    System.out.println("Novo saldo: " + saldo);
+                }
+            } else if (opcao == 3) {
+                System.out.println("Valor recebido: ");
+                double valor = leitura.nextDouble();
+                saldo += valor;
+                System.out.println("Novo saldo: " + saldo);
+            } else if (opcao != 4) {
+                System.out.println("Opção inválida!");
+            }
         }
     }
 }
